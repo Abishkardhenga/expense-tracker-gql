f6387a6eb5603f0c97302b8e90f4bfd236dce009
Abhiskardhenga <abhiskardhenga@Abhiskardhengas-MacBook-Air.local>
2024-06-11 11:39:39 +0545

    passport configured

---
+++ Backend/Passport/passport.config.js
@@ -0,0 +1,55 @@
+import passport from "passport";
+import bycrypt from "bcryptjs"
+import User from "../models/user.model.js"
+import { GraphQLLocalStrategy } from "graphql-passport";
+
+
+ const configurePassport = async()=>{
+    passport.serializeUser((user, done)=>{
+        console.log("Serializing user")
+        done(null, user.id)
+    })
+
+    passport.deserializeUser(async(id,done)=>{
+
+        console.log("Deserailing user")
+const user = User.findById(id)
+done(null, user)
+
+        try {
+            
+        } catch (error) {
+            done(error)
+            
+        }
+    })
+passport.use(
+
+
+new GraphQLLocalStrategy(
+   async (username, password, done)=>{
+
+    try {
+       const user =  User.findOne({username})
+       if(!user){
+        throw new Error("Invalid username or password")
+       }
+       const validPassword = await  bycrypt.compare(password , user.password)
+       if(!validPassword){
+        throw new Error("Invalie username or password")
+       }
+       return done(null, user );
+        
+    } catch (error) {
+        return done(error);
+        
+    }
+
+    }
+)
+    
+)
+}
+
+
+export default configurePassport;
--- Backend/index.js
+++ Backend/index.js
@@ -9,13 +9,40 @@ import mergedResolvers from "./resolvers/index.js";
 import cors from 'cors';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import connectMongoose from './db/connectDb.js';
+import passport from 'passport';
+import session from 'express-session';
+import connectMongo  from 'connect-mongodb-session';
+import {   buildContext } from "graphql-passport"
+import configurePassport from "./Passport/passport.config.js"
 dotenv.config()
+configurePassport()
 
 const app = express();
 
 
 const httpServer = http.createServer(app);
+const MongoDBStore = connectMongo(session)
 
+const store = new MongoDBStore({
+  uri:process.env.MONGODB_URI,
+  collection:"sessions"
+
+})
+store.on("error",(err)=> console.log(err))
+app.use(
+  session({
+    secret:process.env.SESSION_SECRET,
+     resave:false,
+    saveUninitialized:false,
+    cookie:{
+    maxAge:1000*60*60*24*7,
+    httpOnly:true, //this option prevents cross site scripting (XSS) attacks
+    },
+    store:store
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
 
 const server = new ApolloServer({
     typeDefs:mergedTypeDefs,
@@ -27,11 +54,14 @@ const server = new ApolloServer({
 
   app.use(
     '/graphql',
-    cors(),
+    cors({
+      origin:"http://localhost:5173",
+      credentials:true
+    }),
     express.json(),
     
     expressMiddleware(server, {
-      context: async ({ req }) => ({ req }),
+      context: async ({ req , res}) => buildContext({ req , res}),
     }),
   );
   
